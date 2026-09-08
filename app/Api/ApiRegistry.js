453acdab53189200e84cf09bd44b474cdbc775e7
MTDEV <155798314+idev006@users.noreply.github.com>
2026-09-08 12:42:32 +0700

    security(api): register protected self-renew route

--- app/Api/ApiRegistry.js
+++ app/Api/ApiRegistry.js
@@ -29,6 +29,7 @@ Api.ApiRegistry = (() => {
       { method: 'POST', path: '/api/member/me/savings', handler: h.getCurrentSavings, auth: 'line-id-token' },
       { method: 'POST', path: '/api/member/me/loans', handler: h.getCurrentLoans, auth: 'line-id-token' },
       { method: 'POST', path: '/api/member/me/dividends', handler: h.getCurrentDividends, auth: 'line-id-token' },
+      { method: 'POST', path: '/api/member/me/renew', handler: h.renewCurrentMember, auth: 'line-id-token' },
       { method: 'GET', path: '/api/member/profile', handler: h.getProfile, auth: 'line' },
       { method: 'GET', path: '/api/member/savings', handler: h.getSavings, auth: 'line' },
       { method: 'GET', path: '/api/member/loans', handler: h.getLoans, auth: 'line' },
